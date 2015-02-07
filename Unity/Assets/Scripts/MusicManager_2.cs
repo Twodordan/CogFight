62b77f2f250134c9f2714de215ce242ccc844de5
unknown <themorten@gmail.com>
2015-02-08 00:23:32 +0100

    Music manager getting more complete flow control

--- Unity/Assets/Scripts/MusicManager_2.cs
+++ Unity/Assets/Scripts/MusicManager_2.cs
@@ -7,21 +7,22 @@ public class MusicManager_2 : MonoBehaviour {
 
     public float musicDelay = 0.5f;
 
-    public MusicWithInformation beginTrack;
-    public MusicWithInformation endTrack;
+    private int beginTrackIndex = 0;
+    public List<MusicWithInformation> beginTracks;
+    private int endTrackIndex = 0;
+    public List<MusicWithInformation> endTracks;
 
     public List<MusicWithInformation> trackList;
+    public List<MusicWithInformation> foreshadowTracks;
 
     MusicWithInformation currentlyPlayingTrack;
+    List<MusicWithInformation> currentTrackQueue = new List<MusicWithInformation>();
     List<AudioSource> sources = new List<AudioSource>();
 
     int beatNumber = 0;
     double lastBeatTime = 0;
 
-
     IEnumerator Start() {
-        
-
         yield return new WaitForSeconds(musicDelay);
 
         audio.playOnAwake = false;
@@ -39,12 +40,12 @@ public class MusicManager_2 : MonoBehaviour {
 
         
         double initTime = AudioSettings.dspTime;
-        sources[0].clip = beginTrack.clip;
-        sources[0].PlayScheduled(initTime);
-        beginTrack.initTime = initTime;
-        currentlyPlayingTrack = beginTrack;
+        //sources[0].clip = beginTrack.clip;
+        //sources[0].PlayScheduled(initTime);
+        //beginTrack.initTime = initTime;
+        //currentlyPlayingTrack = beginTrack;
 
-        EventManager.Music_NewClip(initTime, sources[0].clip.length); // Declare that we are starting the beginTrack
+        EventManager.Music_NewClip(initTime, 0); // Declare that we are starting the beginTrack
 
         yield break;
     }
@@ -52,24 +53,62 @@ public class MusicManager_2 : MonoBehaviour {
     IEnumerator OnNewClip(double syncTime, double clipLength) {
         double initTime = syncTime + clipLength;
 
-        MusicWithInformation newTrack = trackList[0];
-        AudioSource newSource = sources[1];
-        newSource.clip = newTrack.clip;
-        newSource.PlayScheduled(initTime);
-        newTrack.initTime = initTime;
+        MusicWithInformation newTrack = GetNextTrack();
+        if (newTrack != null) {
 
-        Debug.Log("New clip queued! Name: " + newTrack.name);
+            AudioSource newSource = sources[0];
+            newSource.clip = newTrack.clip;
+            newSource.PlayScheduled(initTime);
+            newTrack.initTime = initTime;
 
-        CycleList(ref sources);
-        CycleList(ref trackList);
+            Debug.Log("New clip queued! Name: " + newTrack.name);
 
-        yield return new WaitForSeconds((float)(initTime - AudioSettings.dspTime - Time.fixedDeltaTime));
+            CycleList(ref sources);
 
-        beatNumber = 0;
-        currentlyPlayingTrack = newTrack;
-        EventManager.Music_NewClip(initTime, newTrack.clip.length);
+            yield return new WaitForSeconds((float)(initTime - AudioSettings.dspTime - Time.fixedDeltaTime));
 
-        yield break;
+            beatNumber = 0;
+            currentlyPlayingTrack = newTrack;
+            EventManager.Music_NewClip(initTime, newTrack.clip.length);
+
+            yield break;
+        } else {
+            Debug.Log("Dunno lol");
+        }
+    }
+
+    MusicWithInformation GetNextTrack() {
+        MusicWithInformation result = null;
+        
+        switch (StateManager.State) {
+            case GameState.Beginning:
+                if (beginTrackIndex > beginTracks.Count - 1) {
+                    EventManager.OnMusic_StartNewClip += QueuedEventSetStatePlaying;
+                    result = trackList[0];
+                    CycleList(ref trackList);
+                } else {
+                    result = beginTracks[beginTrackIndex];
+                    beginTrackIndex++;
+                }
+                break;
+
+            case GameState.Playing:
+                result = trackList[0];
+                CycleList(ref trackList);
+                break;
+
+            case GameState.Ended:
+                if (endTrackIndex > endTracks.Count - 1) {
+                    EventManager.OnMusic_StartNewClip += QueuedEventQuit;
+                    result = null;
+                } else {
+                    result = endTracks[endTrackIndex];
+                    endTrackIndex++;
+                }
+                break;
+        }
+        
+        return result;
     }
 
     void FixedUpdate() {
@@ -87,6 +126,16 @@ public class MusicManager_2 : MonoBehaviour {
 
     void OnGUI() {
         GUI.Label(new Rect(0, 0, 100, 100), ((beatNumber - 1) / 4 + 1).ToString());
+    }
+
+    void QueuedEventSetStatePlaying(double time, double clipLength) {
+        StateManager.State = GameState.Playing;
+        EventManager.OnMusic_StartNewClip -= QueuedEventSetStatePlaying;
+    }
+
+    void QueuedEventQuit(double time, double clipLength) {
+        Debug.Log("QUIT TO MAIN MENU");
+        EventManager.OnMusic_StartNewClip -= QueuedEventQuit;
     }
 
     public static void CycleList<T>(ref List<T> list) {
