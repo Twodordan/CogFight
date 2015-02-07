f8b1afd4383cd343afa540ae7ca8eeeb68e04e17
unknown <themorten@gmail.com>
2015-02-07 13:53:47 +0100

    New files structure and ignore lists

---
+++ Unity/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MusicManager : MonoBehaviour {
+
+    List<AudioClip> musicTracks;
+
+	void Start () {
+	    
+	}
+	
+	void Update () {
+	    
+	}
+}
