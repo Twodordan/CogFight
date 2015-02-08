9063d6ed7657052c0405c75a5307d5e5396759e5
Martin Andersen <martin@danishdragon.com>
2015-02-08 05:28:35 +0100

    Martin stuff, we're now doomed.

---
+++ Unity/Assets/Scripts/ObstacleTrigger.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstacleTrigger : MonoBehaviour {
+
+	private GameObject[] tileArray;
+	public Spike spike;
+	public Material tileMaterial;
+	public Material warningMaterial;
+	private int activeTile;
+
+	void Start() {
+		//Find all the tiles tagged with tiles (the ones beneath the players)
+		tileArray = GameObject.FindGameObjectsWithTag("Tile");
+
+		if (tileArray.Length == 0) {
+			Debug.Log("No game objects are tagged with Tile");
+		}
+	}
+	
+
+	void Awake (){
+		
+		EventManager.OnMusic_ForeshadowBegin += ForeshadowBegin;
+		EventManager.OnMusic_ForeshadowConclusion += ForeshadowConclusion;
+			
+	}
+	
+	
+	void ForeshadowBegin (int n, double m){
+		//Select a random Tile
+		activeTile = Random.Range(0,tileArray.Length-1);
+
+		warnTile((float)m);
+
+	}
+
+	void warnTile(float time){
+		float i = 0.0f;
+		float rate = 1f/time;
+		while (i<1.0f){
+			i += Time.deltaTime*rate;
+			tileArray[activeTile].renderer.material.color = Color.Lerp(tileMaterial.color, warningMaterial.color, i);
+		}
+	}
+
+	void ForeshadowConclusion (int n, double m){
+
+		tileArray[activeTile].renderer.material = tileMaterial;
+
+		spike.ActivateStab(tileArray[activeTile].transform.position.x);
+		
+	}
+
+}
---
+++ Unity/Assets/Scripts/line.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class line : MonoBehaviour {
+
+	private LineRenderer renderer;
+	public Transform[] transforms;
+
+	// Use this for initialization
+	void Start () {
+		renderer = gameObject.GetComponent<LineRenderer>();
+		renderer.SetVertexCount(transforms.Length);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		renderer.SetPosition(0,transforms[0].position);
+		renderer.SetPosition(1,(transforms[0].position-transforms[1].position)*0.5f+transforms[1].position);
+
+
+		float distance = Vector3.Distance(transforms[0].position,transforms[1].position);
+		renderer.SetWidth(0.7f, 1.2f-(distance/10));
+	}
+}
