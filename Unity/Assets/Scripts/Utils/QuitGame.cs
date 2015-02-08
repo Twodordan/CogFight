06a02780d20202e65e75397811599d39c39d4c10
unknown <themorten@gmail.com>
2015-02-08 05:22:17 +0100

    Start menu placeholder

--- Unity/Assets/Scripts/Utils/QuitGame.cs
+++ Unity/Assets/Scripts/Utils/QuitGame.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 public class QuitGame : MonoBehaviour {
 
+    void Start() {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
     void Update() {
         if (Input.GetKeyDown(KeyCode.Escape)) {
             Application.Quit();
